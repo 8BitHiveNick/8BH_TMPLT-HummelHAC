49fb09242c90a10b1baf1dee071c68919c756390
8-Bit Hive Technology - Nick Collins <nick@8bithive.com>
2024-05-02 12:50:44 -0400

    Visual changes

--- js/main.js
+++ js/main.js
@@ -20,3 +20,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const yearSpan = document.getElementById("year");
+  const currentYear = new Date().getFullYear();
+  yearSpan.textContent = currentYear;
+});
